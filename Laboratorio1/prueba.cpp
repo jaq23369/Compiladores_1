edf909e10f3287d855d4c3a5d8e00f3771001062
Joel Jaquez <123708276+jaq23369@users.noreply.github.com>
2026-02-14 20:43:38 -0600

    Lab01

---
+++ Laboratorio1/prueba.cpp
@@ -0,0 +1,19 @@
+// Archivo de prueba
+/* Esto es un comentario de bloque
+que debe ser ignorado por el lexer */
+
+int main() {
+    // Probando números
+    int entero = 42;
+    float pi = 3.14159;
+    double ciencia = 1.6e-19;
+    int hex = 0xFF;
+
+    // Probando strings
+    string saludo = "Hola \"Mundo\"";
+
+    // Probando operadores
+    if (entero >= 10 && hex != 0) {
+        entero = entero + 1;
+    }
+}
